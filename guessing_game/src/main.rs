f529b01a6daca5d341795b190a48e94e68b9856e
RafaZeero <rafaa99@gmail.com>
2023-09-29 00:07:24 -0300

    improve with comments and total tries

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    let mut total_guesses: u32 = 0;
+
     loop {
         println!("Please input your guess.");
 
@@ -16,6 +18,7 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        // Type Result = Either
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -23,14 +26,23 @@ fn main() {
 
         println!("You guessed: {guess}");
 
+        // Match
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("Too small!");
+                total_guesses += 1;
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                total_guesses += 1;
+            }
             Ordering::Equal => {
                 println!("You win!");
+                total_guesses += 1;
                 break;
             }
         }
     }
     println!("The secret number is {secret_number}.");
+    println!("You took {total_guesses} guesses to find it.");
 }
